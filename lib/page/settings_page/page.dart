109cbffa4ed6dbb434b6a4320667262c788a58a1
mclk623 <66455829+mclk623@users.noreply.github.com>
2025-03-01 20:39:13 +0800

    Update page.dart

--- lib/page/settings_page/page.dart
+++ lib/page/settings_page/page.dart
@@ -1,7 +1,6 @@
 import 'package:coriander_player/page/page_scaffold.dart';
 import 'package:coriander_player/page/settings_page/artist_separator_editor.dart';
 import 'package:coriander_player/page/settings_page/check_update.dart';
-import 'package:coriander_player/page/settings_page/create_issue.dart';
 import 'package:coriander_player/page/settings_page/other_settings.dart';
 import 'package:coriander_player/page/settings_page/theme_settings.dart';
 import 'package:flutter/material.dart';
